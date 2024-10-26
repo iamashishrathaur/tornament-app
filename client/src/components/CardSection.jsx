78108ea18151e6de0b8b6f875f2e2c101f1a395f
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2024-10-26 12:31:57 +0530

    remove padding

--- client/src/components/CardSection.jsx
+++ client/src/components/CardSection.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Card, CardContent } from '@mui/material';
 
 const CardSection = ({ activeTab, tabIndex, children }) => (
-  <div hidden={activeTab !== tabIndex}>
+  <div hidden={activeTab !== tabIndex} className='pt-8'>
     <Card>
       <CardContent>{children}</CardContent>
     </Card>
